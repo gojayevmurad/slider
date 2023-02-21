4ef94efd1b0baa4207f3c470daa00783a9df692f
Murad Qocayev <muradqocayevi20@gmail.com>
2023-02-21 21:08:35 +0300

    first slide update

--- js/main.js
+++ js/main.js
@@ -1,36 +1,33 @@
 const prevBtn = document.querySelector(".btn-prev");
 const nextBtn = document.querySelector(".btn-next");
 const slides = document.querySelectorAll(".slide");
-
+const buttons = [prevBtn, nextBtn];
 let currentSlide = 0;
 let latestSlide = slides.length - 1;
 
-slides.forEach((slide,index)=>{
-  slide.style.transform = `translate(${index*100}%)`
-})
-
-nextBtn.addEventListener("click", () => {
-  if (currentSlide === latestSlide) {
-    currentSlide = 0;
-  } else {
-    currentSlide++;
-  }
-
-  slides.forEach((slide, index) => {
-    let calculatePercant = 100 * (index - currentSlide);
-    slide.style.transform = `translateX(${calculatePercant}%)`;
-  });
+slides.forEach((slide, index) => {
+  slide.style.transform = `translate(${index * 100}%)`;
 });
 
-prevBtn.addEventListener("click", () => {
-  if (currentSlide === 0) {
-    currentSlide = latestSlide;
-  } else {
-    currentSlide--;
-  }
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (e.target.value == "-") {
+      if (currentSlide === 0) {
+        currentSlide = latestSlide;
+      } else {
+        currentSlide--;
+      }
+    } else {
+      if (currentSlide === latestSlide) {
+        currentSlide = 0;
+      } else {
+        currentSlide++;
+      }
+    }
 
-  slides.forEach((slide, index) => {
-    let calculatePercant = 100 * (index - currentSlide);
-    slide.style.transform = `translateX(${calculatePercant}%)`;
+    slides.forEach((slide, index) => {
+      let calculatePercent = 100 * (index - currentSlide);
+      slide.style.transform = `translateX(${calculatePercent}%)`;
+    });
   });
 });
