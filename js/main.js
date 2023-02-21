ce94000f6844785d7bbb4068d23ae4f0dbd55b68
Murad Qocayev <muradqocayevi20@gmail.com>
2023-02-21 21:59:47 +0300

    second commit

--- js/main.js
+++ js/main.js
@@ -1,33 +1,82 @@
-const prevBtn = document.querySelector(".btn-prev");
-const nextBtn = document.querySelector(".btn-next");
-const slides = document.querySelectorAll(".slide");
-const buttons = [prevBtn, nextBtn];
-let currentSlide = 0;
-let latestSlide = slides.length - 1;
+//#region First slider
 
-slides.forEach((slide, index) => {
+//! FIRST SLIDER ELEMENTS
+const firstSliderPrevBtn = document.querySelector(".btn-prev");
+const firstSliderNextBtn = document.querySelector(".btn-next");
+const firstSliderSlides = document.querySelectorAll(".slide");
+const firstSliderButtons = [firstSliderPrevBtn, firstSliderNextBtn];
+let firstSliderCurrentSlide = 0;
+let firstSliderLatestSlide = firstSliderSlides.length - 1;
+
+//! FIRST SLIDER FUNCTIONS
+firstSliderSlides.forEach((slide, index) => {
   slide.style.transform = `translate(${index * 100}%)`;
 });
 
-buttons.forEach((button) => {
+firstSliderButtons.forEach((button) => {
   button.addEventListener("click", (e) => {
     if (e.target.value == "-") {
-      if (currentSlide === 0) {
-        currentSlide = latestSlide;
+      if (firstSliderCurrentSlide === 0) {
+        firstSliderCurrentSlide = firstSliderLatestSlide;
       } else {
-        currentSlide--;
+        firstSliderCurrentSlide--;
       }
     } else {
-      if (currentSlide === latestSlide) {
-        currentSlide = 0;
+      if (firstSliderCurrentSlide === firstSliderLatestSlide) {
+        firstSliderCurrentSlide = 0;
       } else {
-        currentSlide++;
+        firstSliderCurrentSlide++;
       }
     }
 
-    slides.forEach((slide, index) => {
-      let calculatePercent = 100 * (index - currentSlide);
+    firstSliderSlides.forEach((slide, index) => {
+      let calculatePercent = 100 * (index - firstSliderCurrentSlide);
       slide.style.transform = `translateX(${calculatePercent}%)`;
     });
   });
 });
+
+// #endregion First slider
+
+//#region Second slider
+
+const secondSliderSlides = document.querySelectorAll(".second--slider__slide");
+const secondSliderPrevBtn = document.querySelector(
+  ".second--slider__btn--prev"
+);
+const secondSliderNextBtn = document.querySelector(
+  ".second--slider__btn--next"
+);
+const secondSliderButtons = [secondSliderNextBtn, secondSliderPrevBtn];
+let secondSliderCurrentSlide = 0;
+let secondSliderLatestSlide = secondSliderSlides.length - 1;
+
+secondSliderSlides.forEach((slide, index) => {
+  slide.style.transform = `translate(${index * 100}%)`;
+});
+
+secondSliderButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (button.value == "-") {
+      if (secondSliderCurrentSlide == 0) {
+        secondSliderCurrentSlide = secondSliderLatestSlide;
+      } else {
+        secondSliderCurrentSlide--;
+      }
+    } else {
+      if (secondSliderCurrentSlide == secondSliderLatestSlide) {
+        secondSliderCurrentSlide = 0;
+      } else {
+        secondSliderCurrentSlide++;
+      }
+    }
+
+    secondSliderSlides.forEach((slide, index) => {
+      let calculatePercent = 100 * (index - secondSliderCurrentSlide);
+      slide.style.transform = `translateX(${calculatePercent}%)`;
+    });
+  });
+});
+
+//#endregion Second slider
+
